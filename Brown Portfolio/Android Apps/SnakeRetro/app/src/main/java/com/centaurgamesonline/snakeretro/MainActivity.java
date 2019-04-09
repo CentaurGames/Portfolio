868da72e67b0bd0bd3e11252304079793ad080f9
Centaur Games <tech.support@centaurgamesonline.com>
2019-04-08 21:12:26 -0700

    Added the Retro Games

---
+++ Brown Portfolio/Android Apps/SnakeRetro/app/src/main/java/com/centaurgamesonline/snakeretro/MainActivity.java
@@ -0,0 +1,27 @@
+package com.centaurgamesonline.snakeretro;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //setContentView(R.layout.activity_main);
+        WebView myWebView = new WebView(MainActivity.this);
+        myWebView.setBackgroundColor(Color.BLACK);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        setContentView(myWebView);
+        myWebView.loadUrl("file:///android_asset/index.html");
+    }
+}
