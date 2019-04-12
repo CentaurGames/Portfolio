58b91b9d9cfebe5491981403ab09d5575ce8c5bf
Centaur Games <48072975+CentaurGames@users.noreply.github.com>
2019-04-11 17:13:51 -0700

    Update MAHJONG_MYCENAEAN.js

--- Mukherjee Portfolio/Web Apps/Mahjong Mycenaean/MAHJONG_MYCENAEAN.js
+++ Mukherjee Portfolio/Web Apps/Mahjong Mycenaean/MAHJONG_MYCENAEAN.js
@@ -1,6 +1,8 @@
+//URLs for the audio.
 var backgroundAudioURL = "http://centaurgamesonline.com/wp-content/uploads/2018/09/Mahjong-Mycenaean-.mp3";
 var swipeAudioURL = "http://centaurgamesonline.com/wp-content/uploads/2018/09/dustyroom_cartoon_swipe_med_pitch.mp3";
 
+//Fetch the canvases and their contexts.
 var scrollPreventCanvas = document.getElementById("scrollPreventCanvas");
 var imageCanvas = document.getElementById("imgCanvas");
 var imageContext = imageCanvas.getContext("2d");
@@ -37,11 +39,13 @@ var isShuffling = 0;
 var backgroundAudioContext, swipeAudioContext, swipeBuffer, backgroundBuffer,  backgroundAudioVolume, swipeAudioVolume, lastSwipeAudioSource, isSwipeAudioLoaded = 0, isBackgroundAudioLoaded = 0, isSwipeAudioLoading = 0, isBackgroundAudioLoading = 0, backgroundIsPlaying = 0;
 var isIOS = 0, isMobile = 0;
 
+//Remove the scrollbars.
 document.documentElement.style.overflow = "hidden";
 document.body.scroll = "no";
 
 function loadBackgroundAudio() {
-if (isBackgroundAudioLoading) {
+	//Loads the background audio for playback.
+	if (isBackgroundAudioLoading) {
 		return;
 	}
 	isBackgroundAudioLoading = 1;
@@ -82,6 +86,7 @@ function loadSwipeAudio() {
 }
 
 function playSwipeSound() {
+	//Loads the swipe audio for playback.
 	if (!isSwipeAudioLoaded) {
 		return;
 	}
@@ -103,6 +108,7 @@ function playSwipeSound() {
 }
 
 function playBackgroundSound() {
+	//Plays the background audio.
 	if (!isBackgroundAudioLoaded) {
 		return;
 	}
@@ -145,6 +151,7 @@ function centerAllCanvases() {
 }
 
 function rescaleCanvases() {
+	//Centers and rescales all the Canvases.
 	window.scrollTo(0,0);
 	scrollPreventCanvas.style.left = "0px";
 	scrollPreventCanvas.style.top = "0px";
@@ -240,6 +247,7 @@ function gameOverSequence() {
 }
 
 function selectedTile() {
+	//Highlight the selected tile.
 	imageCanvas.width = tileWidth;
 	imageCanvas.height = tileHeight;
 	imageContext.save();
@@ -257,7 +265,7 @@ function selectedTile() {
 }
 
 function drawTileImage(index) {
-	//this was to test if our images look right, we don't actually need it in the end program.
+	//this was to test if our images look right.
 	imageContext.drawImage(tileImages[index],0,0);
 }
 
@@ -276,6 +284,7 @@ function drawTiles(index) {
 }
 
 function drawFrontTiles(index) {
+	//Sketch the tiles and load them.
 	if (index >= highlightedTiles.length) {
 		//images are done loading...do next thing here
 		drawShadedTiles(0);
@@ -408,6 +417,7 @@ function drawFrontTiles(index) {
 }
 
 function drawShadedTiles(index) {
+	//Sketch the shaded tiles and load them.
 	if (index >= shadedTiles.length) {
 		//images are done loading...do next thing here
 		drawBackGroundImage();
@@ -430,12 +440,14 @@ function drawShadedTiles(index) {
 }
 
 function drawBackGroundImage() {
+	//Load the background image.
 	backgroundImage = new Image();
 	backgroundImage.src = backgroundImageURL;
 	backgroundImage.onload = selectedTile;
 }
 
 function doesTileMatch(index1,index2) {
+	//Check if any two selected tiles match.
 	//seasons: index 38-41
 	//flowers: index 34-37
 	if (index1 === index2) {
@@ -453,6 +465,7 @@ function doesTileMatch(index1,index2) {
 }
 
 function makeSecondSelection(evt) {
+	//Player selects the second tile.
 	var rect = tileCanvas.getBoundingClientRect();
 	var scale = 1050/backgroundCanvas.width;
 	var x = (evt.clientX || evt.targetTouches[0].clientX) - rect.left;
@@ -502,6 +515,7 @@ function makeSecondSelection(evt) {
 }
 
 function makeFirstSelection(evt) {
+	//Player selects the first tile.
 	evt.preventDefault();
 	if (gameOver) return;
 	if (isIOS && !isSwipeAudioLoaded) {
@@ -549,6 +563,7 @@ function makeFirstSelection(evt) {
 }
 
 function renderGrid() {
+	//This function renders the grid pattern.
 	if (isShuffling) return;
 	tileContext.clearRect(0,0,tileCanvasWidth,tileCanvasHeight);
 	backgroundContext.clearRect(0,0,1050,700);
@@ -593,6 +608,7 @@ function renderGrid() {
 }
 
 function setEventListeners() {
+	//Set all the event listeners for the game.
 	if (isMobile) {
 		scrollPreventCanvas.addEventListener("touchstart",makeFirstSelection);
 	} else {
@@ -605,6 +621,7 @@ function setEventListeners() {
 }
 
 function reshuffleGrid() {
+	//Reshuffles the grid.
 	var needsTileI = [], needsTileJ = [], needsTileK = [], indicesOnBoard = [];
 	for (var i = 0; i < numX; i++) {
 		for (var j = 0; j < numY; j++) {
@@ -635,6 +652,7 @@ function reshuffleGrid() {
 }
 
 function drawReshufflePlacard() {
+	//This function sketches the reshuffle placard.
 	isShuffling = 1;
 	scoreContext.clearRect(0,0,1050,700);
 	scoreContext.strokeStyle = "black";
@@ -654,6 +672,7 @@ function drawReshufflePlacard() {
 }
 
 function checkForShuffle() {
+	//Check if the game is solvable. If not, shuffle the board.
 	if (gameOver) {
 		return;
 	}
@@ -684,6 +703,7 @@ function checkForShuffle() {
 }
 
 function randomizeGrid() {
+	//Randomly distribute the tiles around the grid.
 	var index = 0;
 	for (var i = 0; i < numX; i++) {
 		for (var j = 0; j < numY; j++) {
@@ -705,6 +725,7 @@ function randomizeGrid() {
 }
 
 function createGrid() {
+	//Creates the grid and marks the overall shape of the Mahjong game.
 	imageCanvas.width = 0;
 	imageCanvas.Height = 0;
 	score = 0;
@@ -772,20 +793,8 @@ function createGrid() {
 	randomizeGrid();
 }
 
-function isClickableAtBeginning(i,j,k) {
-	if (k < numZ-1 && grid[i][j][k+1] === -2) {
-		return 0;
-	}
-	if (i === 0 || grid[i-1][j][k] > -2) {
-		return 1;
-	}
-	if (i === numX-1 || grid[i+1][j][k] > -2) {
-		return 1;
-	}
-	return 0;
-}
-
 function isClickable(i,j,k) {
+	//Tells if the tile can be clicked.
 	if (grid[i][j][k] === -1) return 0;
 	if (k < numZ-1 && grid[i][j][k+1] >= 0) {
 		return 0;
@@ -800,6 +809,7 @@ function isClickable(i,j,k) {
 }
 
 function sketch() {
+	//Starts the game.
 	if (/Android|iPhone|iPad|iPod|webOS|Blackberry|IEMobile|Opera Mini/i.test(navigator.userAgent)) isMobile = 1;
 	if (/iPhone|iPad|iPod/i.test(navigator.userAgent) || true) {
 		isIOS = 1;
